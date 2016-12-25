d70da5ad7a77ed118770456144a6d056b0aadcf1
vahid sotudeh <vahidsotudeh@gmail.com>
2016-12-25 14:09:28 +0330

    دو تا کلاس نون دیگه اضافه شد

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     //TODO: Just run operations from here
     public static void main(String[] args) {
 
+
     }
 }
---
+++ src/SangakProducer.java
@@ -0,0 +1,11 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * Created by Microsoft on 25/12/2016.
+ */
+public class SangakProducer extends Thread {
+    //TODO:Sangak bakery
+    Queue<Customer> queue = new PriorityQueue<>(10);
+
+}
