1ee9fa03c5d5829f3d7f74e8fdc2ff394183c5c6
vahid sotudeh <vahidsotudeh@gmail.com>
2017-01-12 15:07:47 +0330

    تایمر

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Timer;
+
 public class Main {
     //TODO: Just run operations from here
     public static void main(String[] args) {
+        Timer timer = new Timer();
+        Bakery bakery=new Bakery();
+        timer.schedule(bakery,0,10000);
 
 
     }
